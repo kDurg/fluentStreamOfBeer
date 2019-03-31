c62cd8f6641536f5bf3840802a375ae1bb12579b
kDurg <kyle.durigan@gmail.com>
2019-03-31 10:52:20 -0600

    Styling to Cooler/ selected beer section

--- fluentstreamofbeer/src/App.js
+++ fluentstreamofbeer/src/App.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
 import BeerCard from './components/BeerCard';
 import BeerCooler from './components/BeerCooler';
 import axios from 'axios';
-import { get } from 'http';
 
 //Program Overview:
-  // Create a program that uses GET, POST and PUT with a defined database. 
-  // Display information about the beer and allow users to add likes to the beers
+// Create a program that uses GET, POST and PUT with a defined database. 
+// Display information about the beer and allow users to add likes to the beers
 
-  //Functionality:
-    // On program load Start Fetch call: GET
-        // update state based on first object in returned array
-        // pass props to BeerCard.js to display results
-      
-    // Let user search for a beer using the ID
-      // on click event, search DB for ID number 
-        // if available - SetState with new data
-        // if not available - return message, ask if they would like to input new beer
-        
-    // Let user input New Beer: PUSH
-      // On Button Click, open up modal with autofilled ID (last ID++), text box for name and starting likes
+//Functionality:
+// On program load Start Fetch call: GET
+// update state based on first object in returned array
+// pass props to BeerCard.js to display results
 
-    // Update Beer Likes: PUT
-      // allow user to add a like to beer
-      // update local state and DB
+// Let user search for a beer using the ID
+// on click event, search DB for ID number 
+// if available - SetState with new data
+// if not available - return message, ask if they would like to input new beer
+
+// Let user input New Beer: PUSH
+// On Button Click, open up modal with autofilled ID (last ID++), text box for name and starting likes
+
+// Update Beer Likes: PUT
+// allow user to add a like to beer
+// update local state and DB
 
 class App extends Component {
   constructor(props) {
@@ -32,52 +32,69 @@ class App extends Component {
     this.state = {
       id: 1,
       name: null,
-      likes: 1
+      likes: 1,
+      beerlist: [],
     }
   }
 
-  getData = () => { //Not working
-    axios({
-      url:"https://beer.fluentcloud.com/v1/beer/", 
-      method: get,
-      headers: {"Content-Type": "application/json"}
-    })
-      .then(res => {
-        console.log(`Axios Call completed: ${res}`)
-      });
-  }
-
-  increaseLikes = () =>{
-    this.setState({
-      likes: this.state.likes +1
-    })
-    console.log('increased likes');
-  }
-
-  decreaseLikes = () =>{
-    this.setState({
-      likes: this.state.likes -1
-    })
-    console.log('decreased likes');
-
-  }
-
-  componentDidMount() {
-    // {this.getData()}
+  getData = () => { //Workaround to get data
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    let  targetUrl = 'https://beer.fluentcloud.com/v1/beer';
+    let targetUrl = 'https://beer.fluentcloud.com/v1/beer';
     fetch(proxyUrl + targetUrl)
       .then(res => res.json())
       .then(data => {
-        const beer = data;
-        const beerNum = (this.state.id)-1;
-        this.setState({
-          id: beer[beerNum].id,
-          name: beer[beerNum].name,
-          likes: beer[beerNum].likes
-        });
+        { this.fillCooler(data) };
+        { this.selectedBeer(data) };
       })
       .catch(err => console.log(err));
+  }
+
+  // axiosGetData = () => { //Not working
+  //   axios({
+  //     url: "https://beer.fluentcloud.com/v1/beer/",
+  //     method: "get",
+  //     dataType: 'jsonp',
+  //     headers: { "Content-Type": "application/json" }
+  //   })
+  //     .then(res => {
+  //       console.log(`Axios Call completed: ${res}`)
+  //     });
+  // }
+
+  fillCooler = (data) => {
+    for (var i = 0; i < data.length; i++) {
+      console.log('Entry ' + [i] + ': ' + data[i].name);
+      
+    }
+    // const allBeersArray = data.map((beer) => <li>{beer}</li>)
+    // console.log(`fillCooler Beer: ${allBeersArray}`);
+  }
+
+  selectedBeer = (data) => {
+    const beerNum = (this.state.id) - 1;
+    this.setState({
+      id: data[beerNum].id,
+      name: data[beerNum].name,
+      likes: data[beerNum].likes
+    });
+  }
+
+  increaseLikes = () => {
+    this.setState({
+      likes: this.state.likes + 1
+    });
+    console.log('increased likes');
+  }
+
+  decreaseLikes = () => {
+    this.setState({
+      likes: this.state.likes - 1
+    });
+    console.log('decreased likes');
+  }
+
+  componentDidMount() {
+    { this.getData() }
   }
 
   render() {
@@ -87,8 +104,18 @@ class App extends Component {
           <h1>FluentStream (of BEER!)</h1>
         </header>
         <body>
-          {/* <BeerCooler /> */}
-          <BeerCard id={this.state.id} name={this.state.name} likes={this.state.likes} increaseLikes={this.increaseLikes} decreaseLikes={this.decreaseLikes}/>
+          <Container>
+            <Row>
+              <Col md="1" />
+              <Col md="7">
+                <BeerCooler name={this.state.beerlist.name} likes={this.state.beerlist.likes} />
+              </Col>
+              <Col md="auto">
+                <BeerCard id={this.state.id} name={this.state.name} likes={this.state.likes} increaseLikes={this.increaseLikes} decreaseLikes={this.decreaseLikes} />
+              </Col>
+              <Col md="1" />
+            </Row>
+          </Container>
         </body>
       </div>
     );
--- fluentstreamofbeer/src/components/BeerCooler.js
+++ fluentstreamofbeer/src/components/BeerCooler.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Table } from 'reactstrap';
 
 const BeerCooler = props => {
     return (
         <>
             <h2>Juliette's Beer Cooler</h2>
-            <hr />
-            <ul>
-                <li>{props.name}</li>
-            </ul>
+            <Table hover>
+                <thead>
+                    <tr>
+                        <th>ID#</th>
+                        <th>Beer Name</th>
+                        <th>Likes</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">{props.id}</th>
+                        <td>{props.name}</td>
+                        <td>{props.likes}</td>
+                    </tr>
+                </tbody>
+            </Table>
         </>
     )
 }
